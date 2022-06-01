0bb73a4347bb8e4b4d606f54eda68b60fd61c3e6
Juan Rodriguez Jimenez <juanorro@mac-juanorro.local>
2022-06-01 22:37:14 +0200

    delete tweet

--- pages/[name]/status/[id]/index.js
+++ pages/[name]/status/[id]/index.js
@@ -1,10 +1,43 @@
 import { Tweet } from "components/Tweet";
 import { getTweet } from 'lib/data';
 import prisma from "lib/prisma";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const SingleTweet = ({ tweet }) => {
+
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
   return (
-    <Tweet tweet={ tweet } />
+    <div>
+      <Tweet tweet={ tweet } />
+  
+      { session && session.user.email === tweet.author.email && (
+        <div className="flex-1 py-2 m-2 text-center">
+          <a 
+            href="#"
+            className="flex item-center w-12 px-3 py-2 mt-1 text-base font-medium leading-6 text-gray-500 rounded-full group hover:bg-color-accent-hover hover:color-accent-hover"
+            onClick={ async() => {
+              const res = await fetch('/api/tweet', {
+                body: JSON.stringify({
+                  id: tweet.id,
+                }),
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                method: 'DELETE',
+              });
+  
+              if(res.status === 401) return alert('Unauthorized');
+              if(res.status === 200) return router.push('/home');
+            }}
+          >
+            delete
+          </a>
+        </div>
+      )}
+    </div>
   );
 };
 
